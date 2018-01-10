5bd8694d464d44e0991e3f33f4c57f7fff140872
linzhennan <598296448@qq.com>
2018-01-10 16:54:07 +0800

    problem1修改 problem2完成

--- mainclass.java
+++ mainclass.java
@@ -1,8 +1,15 @@
 
 public class mainclass {
 	public static void main(String[] args){
-		problem1 p1 = new problem1();
-		p1.GetProblem();
-		p1.Problem();
+//		problem1 p1 = new problem1();
+//		p1.GetProblem();
+//		p1.Problem();
+//		
+
+
+		
+		problem2 p2=new problem2();
+		p2.GetProblem();
+		p2.Problem();
 	}
 }
--- problem1.java
+++ problem1.java
@@ -1,36 +1,27 @@
 
-import java.util.Scanner;
-
 public class problem1 {
-	public void GetProblem(){
+	public void GetProblem() {
 		System.out.println("亲密数：假设有a、b两个数，若a的所有因子之和等于b,b的所有因子之和等于a，并且a不等于b，则称a和b是一对亲密数。如284和220就是一对亲密数。");
 	}
-	private int SumOfFactors(int number){	
-		int i=1,half=number/2;
-		int sum=1;  //1为任何数质因子
-		/// number/4获取两个因子
-		
-		while(++i<=half){
-			if(0==number%i){
-				sum+=i;
+
+	private int SumOfFactors(int number) {
+		int i = 1, half = number / 2;
+		int sum = 1; // 1为任何数质因子
+
+		while (++i <= half) {
+			if (0 == number % i) {
+				sum += i;
 			}
 		}
 		return sum;
 	}
-	public void Problem(){
-		Scanner sc=new Scanner(System.in);
-		
-		System.out.println("输入a的值：");
-		int num1 =Integer.parseInt(sc.nextLine());
-		System.out.println("输入b的值：");
-		int num2=Integer.parseInt(sc.nextLine());
-		
-		if(num1!=num2&&SumOfFactors(num1)==num2&&num1==SumOfFactors(num2)){
-			System.out.println(num1+" 和 "+num2+"是一对亲密数");
+
+	public void Problem() {
+		for (int num1 = 1; num1 < 10000; num1++) {
+			int num2 = SumOfFactors(num1);
+			if (num2 < num1)
+				if (num1 == SumOfFactors(num2))
+					System.out.println(num1 + " 和 " + num2 + "是亲密数");
 		}
-		else{
-			System.out.println(num1+" 和 "+num2+"不是一对亲密数");
-		}
-		
 	}
 }
